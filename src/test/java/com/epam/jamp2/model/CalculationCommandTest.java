0acefbfc00eafd26052549cf3cd159d2811b7a7f
Alexey Matyas <matias.alexey@gmail.com>
2016-12-07 09:38:27 +0800

    Add hamcrest small demo

--- src/test/java/com/epam/jamp2/model/CalculationCommandTest.java
+++ src/test/java/com/epam/jamp2/model/CalculationCommandTest.java
@@ -6,6 +6,9 @@ import java.math.BigDecimal;
 
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 /**
  * Created by Alexey on 05.12.2016.
@@ -15,7 +18,7 @@ public class CalculationCommandTest {
     public void shouldParseFromString() throws CommandFormatException {
         CalculationCommand parsedCommand = CalculationCommand.parseFromString("usd100.5+200.99654=");
         assertEquals("usd", parsedCommand.getLeftOperand().getCurrencyCode().get());
-        assertTrue(parsedCommand.getLeftOperand().getValue().equals(BigDecimal.valueOf(100.5)));
+        assertThat(parsedCommand.getLeftOperand().getValue(), is(equalTo(BigDecimal.valueOf(100.5))));
         assertTrue(!parsedCommand.getRightOperand().getCurrencyCode().isPresent());
         assertTrue(parsedCommand.getRightOperand().getValue().equals(BigDecimal.valueOf(200.99654)));
         assertTrue(parsedCommand.getOperation().equals(Operation.ADD));
